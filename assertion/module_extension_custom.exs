50368228aa254cd103fd8e23f7666121937271ea
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2016-04-05 21:41:13 +0200

    test macro + module attr

--- assertion/module_extension_custom.exs
+++ assertion/module_extension_custom.exs
@@ -3,9 +3,19 @@ defmodule Assertion do
     quote do
       import unquote(__MODULE__)
 
+      Module.register_attribute __MODULE__, :tests, accumulate: true
+
       def run do
-        IO.puts "Running the tests..."
+        IO.puts "Running the tests... #{inspect @tests}"
       end
+    end
+  end
+
+  defmacro test(description, do: test_block) do
+    test_func = String.to_atom(description)
+    quote do
+      @tests {unquote(test_func), unquote(description)}
+      def unquote(test_func)(), do: unquote(test_block)
     end
   end
 end
