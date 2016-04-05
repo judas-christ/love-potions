b1b91eaa18946b6e84e7631db78aa01c2d1beb9f
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2016-04-05 20:45:58 +0200

    custom extend

--- assertion/assertion.exs
+++ assertion/assertion.exs
@@ -1,12 +1,12 @@
-defmodule Assertions do
+defmodule Assertion do
   defmacro assert({operator, _, [lhs, rhs]}) do
     quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
-      Assertions.Test.assert operator, lhs, rhs
+      Assertion.Test.assert operator, lhs, rhs
     end
   end
 end
 
-defmodule Assertions.Test do
+defmodule Assertion.Test do
   def assert(:==, lhs, rhs) when lhs == rhs do
     IO.write "."
   end
