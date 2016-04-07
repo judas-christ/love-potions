94f8e0e253e8fb62ed01af7062333c01837d682b
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2016-04-07 21:59:43 +0200

    Fixed spelling of valid_type? + added external resource module attribute

--- mimes/mime.exs
+++ mimes/mime.exs
@@ -1,5 +1,8 @@
 defmodule Mime do
-  for line <- File.stream!(Path.join([__DIR__, "mimes.txt"]), [], :line) do
+
+  @external_resource mimes_path = Path.join([__DIR__, "mimes.txt"])
+
+  for line <- File.stream!(mimes_path, [], :line) do
     [type, rest] = line |> String.split("\t") |> Enum.map(&String.strip/1)
     extensions = String.split(rest, ~r{,\s?})
 
@@ -9,5 +12,5 @@ defmodule Mime do
 
   def exts_from_type(_type), do: []
   def type_from_ext(_ext), do: nil
-  def vaild_type?(type), do: exts_from_type(type) |> Enum.any?
+  def valid_type?(type), do: exts_from_type(type) |> Enum.any?
 end
