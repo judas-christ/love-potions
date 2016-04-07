695cdcb115f1616b288dc282e7c48a2098590541
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2016-04-07 22:16:48 +0200

    step 3

--- translator/translator.exs
+++ translator/translator.exs
@@ -20,5 +20,18 @@ defmodule Translator do
 
   def compile(translations) do
     # TODO: generate functions
+    translations_ast = for {locale, mappings} <- translations do
+      deftranslations(locale, "", mappings)
+    end
+
+    quote do
+      def t(locale, path, bindings \\ [])
+      unquote(translations_ast)
+      def t(_locale, _path, _bindings), do: {:error, :no_translation}
+    end
+  end
+
+  defp deftranslations(locales, current_path, mappings) do
+    #TODO: ast for t/3 functions
   end
 end
