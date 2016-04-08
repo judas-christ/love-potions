1997a78edbe0a322cf5f43c087b2fa1f632b94ef
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2016-04-08 22:10:38 +0200

    translator tests

---
+++ translator/translator_test.exs
@@ -0,0 +1,63 @@
+ExUnit.start
+Code.require_file("translator.exs", __DIR__)
+
+defmodule TranslatorTest do
+  use ExUnit.Case
+
+  defmodule I18n do
+    use Translator
+
+    locale "en", [
+      foo: "bar",
+      flash: [
+        notice: [
+          alert: "Alert!",
+          hello: "hello %{first} %{last}!"
+        ]
+      ],
+      users: [
+        title: "Users",
+        profile: [
+          title: "Profiles"
+        ]
+      ]
+    ]
+
+    locale "fr", [
+      flash: [
+        notice: [
+          hello: "salut %{first} %{last}!"
+        ]
+      ]
+    ]
+  end
+
+  test "it recursively wals translations tree" do
+    assert I18n.t("en", "users.title") == "Users"
+    assert I18n.t("en", "users.profile.title") == "Profiles"
+  end
+
+  test "it handles translations at root level" do
+     assert I18n.t("en", "foo") == "bar"
+  end
+
+  test "multiple locales" do
+    assert I18n.t("fr", "flash.notice.hello", first: "Bah", last: "Smeh") == "salut Bah Smeh!"
+  end
+
+  test "it interpolates" do
+    assert I18n.t("en", "flash.notice.hello", first: "Bah", last: "Smeh") == "hello Bah Smeh!"
+  end
+
+  test "t/3 raises KeyError when bindings not provided" do
+    assert_raise KeyError, fn -> I18n.t("en", "flash.notice.hello") end
+  end
+
+  test "t/3 returns {:error, :no_translation} when translation missing" do
+    assert I18n.t("en", "flash.non_existent") == {:error, :no_translation}
+  end
+
+  test "converts interpolated values to strings" do
+    assert I18n.t("fr", "flash.notice.hello", first: 123, last: 456) == "salut 123 456!"
+  end
+end
