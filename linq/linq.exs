39a43dca4c66b5e8028759c8a81d97a8c0562956
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2016-04-11 23:11:25 +0200

    hypothetical linq test

--- linq/linq.exs
+++ linq/linq.exs
@@ -1,38 +1,45 @@
 defmodule Linq do
-  defmacro from({:in, _context, [{what, _, nil}, {:__aliases__, aliases, [from_where]}]} = ast, opts \\ []) do
+  defmacro from({:in, _context, [{what, _, nil}, {:__aliases__, _aliases, [from_where]}]}, opts \\ []) do
     # IO.inspect ast
     # IO.inspect opts
-    table_name = from_where
+
+    # table_name = from_where
     table_alias = to_string(what)
     where_clause = Keyword.get(opts, :where) |> get_where_clauses(table_alias)
     if byte_size(where_clause) > 0 do
       where_clause = " where " <> where_clause
     end
-    IO.puts "select"
-    IO.inspect Keyword.get(opts, :select)
+    # IO.puts "select"
+    # IO.inspect Keyword.get(opts, :select)
     select_clause = Keyword.get(opts, :select) |> get_select_clause(table_alias)
     quote do
-      "select #{unquote(select_clause)} from #{unquote(table_name)} as #{unquote(table_alias)}#{unquote(where_clause)}"
+      "select #{unquote(select_clause)} from #{:"Elixir.#{unquote(from_where)}".table_name} as #{unquote(table_alias)}#{unquote(where_clause)}"
     end
   end
 
   defmacro schema(options, do: block) do
     table_name = options[:table_name]
-    IO.inspect block
+    # IO.inspect block
 
     struct_def = get_struct_def(block)
     quote do
       @table_name unquote(table_name) || to_string(__MODULE__)
-      IO.puts(@table_name)
-
+      # IO.puts(@table_name)
       defstruct unquote(struct_def)
+      def table_name do
+        @table_name
+      end
     end
   end
 
   def get_struct_def({:__block__, _, ast}) do
-    for line <- ast, {:field, _field_line, [field_name, field_type | opts]} = line, into: [] do
+    for line <- ast, {:field, _field_line, [field_name, _field_type | _opts]} = line, into: [] do
+      # TODO: get default from opts
       {field_name, nil}
     end
+  end
+  def get_struct_def({:field, _, [field_name, _field_type | _opts]}) do
+    [{field_name, nil}]
   end
 
   def get_where_clauses(nil, _), do: ""
@@ -41,7 +48,7 @@ defmodule Linq do
     rhs = get_where_clauses(rhs, table_alias)
     "#{lhs}.#{rhs}"
   end
-  def get_where_clauses({atom, _, nil}, table_alias) do
+  def get_where_clauses({_atom, _, nil}, table_alias) do
     table_alias
   end
   def get_where_clauses(string, _) when is_bitstring(string) do
@@ -65,7 +72,7 @@ defmodule Linq do
   def get_select_clause(nil, table_alias), do: "#{table_alias}.*"
   def get_select_clause(fields, table_alias) do
     for item <- fields,
-      {dest, src} = item,
+      {_dest, src} = item,
       {{:., _, [_, field_name]}, _, _} = src,
       into: [] do
       # IO.inspect src
@@ -74,31 +81,31 @@ defmodule Linq do
   end
 end
 
-defmodule Fejk do
-  import Linq
+# defmodule Fejk do
+#   import Linq
 
-  schema table_name: "fakes" do
-    field :name, :string
-    field :age, :integer
-  end
+#   schema table_name: "fakes" do
+#     field :name, :string
+#     field :age, :integer
+#   end
 
-  def all do
-    from fejk in Fejk
-  end
+#   def all do
+#     from fejk in Fejk
+#   end
 
-  def some do
-    from fejk in Fejk,
-      where: fejk.age > 0 and fejk.age < 30 and fejk.name != "bah"
-  end
+#   def some do
+#     from fejk in Fejk,
+#       where: fejk.age > 0 and fejk.age < 30 and fejk.name != "bah"
+#   end
 
-  def map do
-    from fejk in Fejk,
-      select: [name: fejk.name]
-  end
+#   def map do
+#     from fejk in Fejk,
+#       select: [name: fejk.name]
+#   end
 
-  def map_some do
-    from fejk in Fejk,
-      where: fejk.age == 40,
-      select: [name: fejk.name]
-  end
-end
+#   def map_some do
+#     from fejk in Fejk,
+#       where: fejk.age == 40,
+#       select: [name: fejk.name]
+#   end
+# end
--- linq/linq_test.exs
+++ linq/linq_test.exs
@@ -4,15 +4,40 @@ Code.require_file("linq.exs", __DIR__)
 defmodule LinqTest do
   use ExUnit.Case
 
-  defmodule Entity do
-
+  test "table name" do
+    assert String.contains?(Entity.all, "from entities as")
   end
 
-  test "from something" do
-    import Linq
-
-    ast = quote do: from entity in Entity,
-      select: entity
-    IO.inspect ast
+  test "filter" do
+    assert String.contains?(Entity.some, "name <> \"bah\"")
   end
 end
+
+defmodule Entity do
+    import Linq
+
+    schema table_name: "entities" do
+      field :name, :string
+      field :age, :integer
+    end
+
+    def all do
+      from fejk in Entity
+    end
+
+    def some do
+      from entity in Entity,
+        where: entity.age > 0 and entity.age < 30 and entity.name != "bah"
+    end
+
+    def map do
+      from entity in Entity,
+        select: [name: entity.name]
+    end
+
+    def map_some do
+      from entity in Entity,
+        where: entity.age == 40,
+        select: [name: entity.name]
+    end
+  end
